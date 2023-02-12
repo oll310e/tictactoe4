93a7df766915cc31529ddbbfe591a1849c4421f0
oll310e <o.anderzen@gmail.com>
2023-02-12 18:44:06 +0100

    Add functions

--- script.js
+++ script.js
@@ -8,30 +8,71 @@
   }
 })();
 
-function Player(sign, name, turn) {
+function SetUpDomQuery() {
+  return {
+    gameboard: document.querySelector(".gameboard"),
+    startBtn: document.querySelector(".start"),
+    playersForm: document.querySelector(".players-form"),
+    playerName: document.querySelector("#name"),
+    playerSign: document.querySelector("#sign"),
+    playerTurn: document.querySelector("#turn"),
+  };
+}
+
+const DOM = SetUpDomQuery();
+
+//
+
+function Player(sign, name) {
   this.sign = sign;
   this.name = name;
-  this.turn = turn;
+  this.addSign = function (e) {
+    console.log(e.target, this.sign);
+  };
 }
 
 function addPlayers() {
   togglePlayersForm();
 }
 
-function togglePlayersForm() {}
-
-function SetUpDomQuery() {
-  return {
-    gameboard: document.querySelector(".gameboard"),
-    startBtn: document.querySelector("start"),
-  };
+function togglePlayersForm() {
+  if (DOM.playersForm.classList.contains("hidden")) {
+    DOM.playersForm.classList.toggle("hidden");
+  }
 }
 
-const DOM = SetUpDomQuery();
+function emptyForm() {
+  DOM.playerName.value = "";
+  DOM.playerSign.value = "";
+}
 
 //Events
+
+let gameStarted = false;
+
 DOM.startBtn.addEventListener("click", (e) => {
   addPlayers();
+  gameStarted = true;
+});
+
+DOM.playersForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let name = DOM.playerName.value;
+  let sign = DOM.playerSign.value;
+
+  let newPlayer = new Player(name, sign);
+
+  DOM.playersForm.classList.toggle("hidden");
+
+  emptyForm();
+
+  console.log(newPlayer);
+});
+
+DOM.gameboard.addEventListener("click", (e) => {
+  if (e.target.classList.contains("cell") && gameStarted) {
+    turnPlayer.addSign(e);
+  }
 });
 
 //start game
